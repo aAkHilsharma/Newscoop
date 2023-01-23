1b303563564b3883ca79a3118215a70bee814851
Akhil Sharma <akhilsharma6721@gmail.com>
2023-01-23 21:06:21 +0530

    modified newsItem

--- src/Components/News.js
+++ src/Components/News.js
@@ -7,7 +7,7 @@ export default class News extends Component{
             <h1>Newscoop - Top Headlines</h1>
             <div className="row">
                 <div className="col-md-4">
-                    <NewsItem title="myTitle" description="myDesc"/>
+                    <NewsItem title="myTitle" description="myDesc" imageUrl="https://live-production.wcms.abc-cdn.net.au/d718618805e29f723e0fa707421b0079?impolicy=wcms_crop_resize&cropH=2268&cropW=4032&xPos=0&yPos=0&width=862&height=485"/>
                 </div>
                 <div className="col-md-4">
                     <NewsItem title="myTitle" description="myDesc"/>
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,11 +2,11 @@ import { Component } from "react";
 
 export default class NewsItem extends Component{
     render(){
-        let {title, description} = this.props;
+        let {title, description, imageUrl} = this.props;
         return(
-            <div>
+            <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
-                    <img src="https://live-production.wcms.abc-cdn.net.au/d718618805e29f723e0fa707421b0079?impolicy=wcms_crop_resize&cropH=2268&cropW=4032&xPos=0&yPos=0&width=862&height=485" className="card-img-top" alt="..." />
+                    <img src={imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
