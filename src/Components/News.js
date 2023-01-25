5d2f2949a1fbe694e5583caea6016309bd5277cd
akhil <akhilsharma6721@gmail.com>
2023-01-25 20:10:12 +0530

    added pages

--- src/Components/News.js
+++ src/Components/News.js
@@ -5,7 +5,8 @@ export default class News extends Component{
         super();
         this.state = {
             articles: [],
-            loading: false
+            loading: false,
+            page: 1
         }
     }
     async componentDidMount() {
@@ -18,6 +19,22 @@ export default class News extends Component{
          input?.length > 45? `${input.substring(0, 45)}...` : input;
     reduce = (input) =>
          input?.length > 100? `${input.substring(0, 100)}...` : input;
+    handleNextClick = async()=>{
+        let url  = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=7385c9251e4543b1a42fdeacdbbd1a11&page=${this.state.page + 1}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState ({articles : parsedData.articles,
+            page : this.state.page+ 1
+        });
+    }
+    handlePrevClick = async()=>{
+        let url  = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=7385c9251e4543b1a42fdeacdbbd1a11&page=${this.state.page - 1}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState ({articles : parsedData.articles,
+            page: this.state.page - 1
+        });
+    }
     render(){
         return(
             <div className="container my-3">
@@ -26,6 +43,10 @@ export default class News extends Component{
                 {this.state.articles.map((element)=>{return <div className="col-md-4" key={element.url}>
                     <NewsItem title={this.truncate(element.title)} description={this.reduce(element.description)} imageUrl={element.urlToImage} newsUrl={element.url}/>
                 </div>})}
+            </div>
+            <div className="container d-flex justify-content-between">
+                <button disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+                <button className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
             </div>
             </div>
         )
