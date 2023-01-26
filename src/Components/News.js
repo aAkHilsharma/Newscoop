226f57b67fd70cf07ed799c1e62f7dd6feb07a52
akhil <akhilsharma6721@gmail.com>
2023-01-26 23:05:24 +0530

    added loading spinner

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import NewsItem from "../Components/NewsItem"
+import Spinner from "./Spinner";
 export default class News extends Component{
     constructor(){
         super();
@@ -11,9 +12,14 @@ export default class News extends Component{
     }
     async componentDidMount() {
         let url  = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=7385c9251e4543b1a42fdeacdbbd1a11&pageSize=${this.props.pageSize}`;
+        this.setState({loading : true})
         let data = await fetch(url);
         let parsedData = await data.json();
-        this.setState ({articles : parsedData.articles, totalResults: parsedData.totalResults});
+        this.setState({
+            articles : parsedData.articles,
+            totalResults: parsedData.totalResults,
+            loading : false
+        });
     }
     truncate = (input) =>
          input?.length > 45? `${input.substring(0, 45)}...` : input;
@@ -22,27 +28,32 @@ export default class News extends Component{
     handleNextClick = async()=>{
         if(!(this.state.page+1 > Math.ceil(this.state.totalResults / 20))){
             let url  = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=7385c9251e4543b1a42fdeacdbbd1a11&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            this.setState({loading : true})
             let data = await fetch(url);
             let parsedData = await data.json();
             this.setState ({articles : parsedData.articles,
-            page : this.state.page+ 1
+            page : this.state.page+ 1,
+            loading : false
         });
         }
     }
     handlePrevClick = async()=>{
         let url  = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=7385c9251e4543b1a42fdeacdbbd1a11&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        this.setState({loading : true})
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState ({articles : parsedData.articles,
-            page: this.state.page - 1
+            page: this.state.page - 1,
+            loading : false
         });
     }
     render(){
         return(
             <div className="container my-3">
             <h1 className="text-center">Newscoop - Top Headlines</h1>
+            {this.state.loading && <Spinner />}
             <div className="row">
-                {this.state.articles.map((element)=>{return <div className="col-md-4" key={element.url}>
+                {!this.state.loading && this.state.articles.map((element)=>{return <div className="col-md-4" key={element.url}>
                     <NewsItem title={this.truncate(element.title)} description={this.reduce(element.description)} imageUrl={element.urlToImage} newsUrl={element.url}/>
                 </div>})}
             </div>
---
+++ src/Components/Spinner.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import loading from "../Spinner-1s-200px.gif"
+export default class Spinner extends Component{
+    render(){
+        return(
+            <div className="text-center">
+                <img src={loading} alt="alt" />
+            </div>
+        )
+    }
+}
