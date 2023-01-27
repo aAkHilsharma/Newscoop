bc7a77bb1e7e4c43100e7819a5a8e3d41c73111f
akhil <akhilsharma6721@gmail.com>
2023-01-27 18:34:40 +0530

    formatted date

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -10,7 +10,7 @@ export default class NewsItem extends Component{
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <p class="card-text"><small class="text-muted">By {!author?"Unknown":author} on {date}</small></p>
+                        <p class="card-text"><small class="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
                         <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
